3472f663a45d043424dbe9b2aaf98f69704abc02
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-04 11:17:44 -0500

    Add project files.

---
+++ LoppyForArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace LoppyForArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<type>[<declares array] = new <same type> [<size>]
+            string[] input = new string[10];
+
+            string[] otherInput = { "rock", "rock", "rock" }; //another way to create an array
+
+            //for loop (variable declaration/being set; loop consition; iterating process)
+            for (int counter = 0; counter < 3; counter++)
+            {
+                input[counter] = ReadLine();
+                Clear();
+            }
+
+            //can declare variable 'counter' again because prev one was only made and used in prev for loop
+            for (int counter = 2; counter >= 0; counter--)
+            {
+                WriteLine("Player{0} chose {1}", counter + 1, input[counter]);
+            }
+        }
+    }
+}
